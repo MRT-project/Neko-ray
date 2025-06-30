2dust/v2rayNG

Added setting option for VPN interface address
https://github.com/2dust/v2rayNG/issues/4641

V2rayNG/app/src/main/java/com/v2ray/ang/dto/VpnInterfaceAddressConfig.kt
@@ -1,0 +1,39 @@
+package com.v2ray.ang.dto
+
+/**
+ * VPN interface address configuration enum class
+ * Defines predefined IPv4 and IPv6 address pairs for VPN TUN interface configuration.
+ * Each option provides client and router addresses to establish point-to-point VPN tunnels.
+ */
+enum class VpnInterfaceAddressConfig(
+    val displayName: String,
+    val ipv4Client: String,
+    val ipv4Router: String,
+    val ipv6Client: String,
+    val ipv6Router: String
+) {
+    OPTION_1("10.10.14.x", "10.10.14.1", "10.10.14.2", "fc00::10:10:14:1", "fc00::10:10:14:2"),
+    OPTION_2("10.1.0.x", "10.1.0.1", "10.1.0.2", "fc00::10:1:0:1", "fc00::10:1:0:2"),
+    OPTION_3("10.0.0.x", "10.0.0.1", "10.0.0.2", "fc00::10:0:0:1", "fc00::10:0:0:2"),
+    OPTION_4("172.31.0.x", "172.31.0.1", "172.31.0.2", "fc00::172:31:0:1", "fc00::172:31:0:2"),
+    OPTION_5("172.20.0.x", "172.20.0.1", "172.20.0.2", "fc00::172:20:0:1", "fc00::172:20:0:2"),
+    OPTION_6("172.16.0.x", "172.16.0.1", "172.16.0.2", "fc00::172:16:0:1", "fc00::172:16:0:2"),
+    OPTION_7("192.168.100.x", "192.168.100.1", "192.168.100.2", "fc00::192:168:100:1", "fc00::192:168:100:2");
+
+    companion object {
+        /**
+         * Retrieves the VPN interface address configuration based on the specified index.
+         *
+         * @param index The configuration index (0-based) corresponding to user selection
+         * @return The VpnInterfaceAddressConfig instance at the specified index,
+         *         or OPTION_1 (default) if the index is out of bounds
+         */
+        fun getConfigByIndex(index: Int): VpnInterfaceAddressConfig {
+            return if (index in values().indices) {
+                values()[index]
+            } else {
+                OPTION_1 // Default to the first configuration
+            }
+        }
+    }
+}

V2rayNG/app/src/main/java/com/v2ray/ang/handler/SettingsManager.kt
@@ -16,6 +16,7 @@ import com.v2ray.ang.dto.ProfileItem
 import com.v2ray.ang.dto.RoutingType
 import com.v2ray.ang.dto.RulesetItem
 import com.v2ray.ang.dto.V2rayConfig
+import com.v2ray.ang.dto.VpnInterfaceAddressConfig
 import com.v2ray.ang.handler.MmkvManager.decodeServerConfig
 import com.v2ray.ang.handler.MmkvManager.decodeServerList
 import com.v2ray.ang.util.JsonUtil
@@ -355,5 +356,18 @@ object SettingsManager {
             "1" -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
             "2" -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
         }
+    }
+
+    /**
+     * Retrieves the currently selected VPN interface address configuration.
+     * This method reads the user's preference for VPN interface addressing and returns
+     * the corresponding configuration containing IPv4 and IPv6 addresses.
+     *
+     * @return The selected VpnInterfaceAddressConfig instance, or the default configuration
+     *         if no valid selection is found or if the stored index is invalid.
+     */
+    fun getCurrentVpnInterfaceAddressConfig(): VpnInterfaceAddressConfig {
+        val selectedIndex = MmkvManager.decodeSettingsString(AppConfig.PREF_VPN_INTERFACE_ADDRESS_CONFIG_INDEX, "0")?.toInt()
+        return VpnInterfaceAddressConfig.getConfigByIndex(selectedIndex ?: 0)
     }
 }

MRT-project/Neko-ray

V2rayNG/app/src/main/java/com/neko/v2ray/dto/VpnInterfaceAddressConfig.kt
@@ -1,0 +1,39 @@
+package com.neko.v2ray.dto
+
+/**
+ * VPN interface address configuration enum class
+ * Defines predefined IPv4 and IPv6 address pairs for VPN TUN interface configuration.
+ * Each option provides client and router addresses to establish point-to-point VPN tunnels.
+ */
+enum class VpnInterfaceAddressConfig(
+    val displayName: String,
+    val ipv4Client: String,
+    val ipv4Router: String,
+    val ipv6Client: String,
+    val ipv6Router: String
+) {
+    OPTION_1("10.10.14.x", "10.10.14.1", "10.10.14.2", "fc00::10:10:14:1", "fc00::10:10:14:2"),
+    OPTION_2("10.1.0.x", "10.1.0.1", "10.1.0.2", "fc00::10:1:0:1", "fc00::10:1:0:2"),
+    OPTION_3("10.0.0.x", "10.0.0.1", "10.0.0.2", "fc00::10:0:0:1", "fc00::10:0:0:2"),
+    OPTION_4("172.31.0.x", "172.31.0.1", "172.31.0.2", "fc00::172:31:0:1", "fc00::172:31:0:2"),
+    OPTION_5("172.20.0.x", "172.20.0.1", "172.20.0.2", "fc00::172:20:0:1", "fc00::172:20:0:2"),
+    OPTION_6("172.16.0.x", "172.16.0.1", "172.16.0.2", "fc00::172:16:0:1", "fc00::172:16:0:2"),
+    OPTION_7("192.168.100.x", "192.168.100.1", "192.168.100.2", "fc00::192:168:100:1", "fc00::192:168:100:2");
+
+    companion object {
+        /**
+         * Retrieves the VPN interface address configuration based on the specified index.
+         *
+         * @param index The configuration index (0-based) corresponding to user selection
+         * @return The VpnInterfaceAddressConfig instance at the specified index,
+         *         or OPTION_1 (default) if the index is out of bounds
+         */
+        fun getConfigByIndex(index: Int): VpnInterfaceAddressConfig {
+            return if (index in values().indices) {
+                values()[index]
+            } else {
+                OPTION_1 // Default to the first configuration
+            }
+        }
+    }
+}

V2rayNG/app/src/main/java/com/neko/v2ray/handler/SettingsManager.kt
@@ -5,6 +5,7 @@ import android.content.res.AssetManager
 import android.text.TextUtils
 import android.util.Log
 import androidx.appcompat.app.AppCompatDelegate
+<<<<<<< HEAD:V2rayNG/app/src/main/java/com/neko/v2ray/handler/SettingsManager.kt
 import com.neko.v2ray.AppConfig
 import com.neko.v2ray.AppConfig.ANG_PACKAGE
 import com.neko.v2ray.AppConfig.GEOIP_PRIVATE
@@ -20,6 +21,24 @@ import com.neko.v2ray.handler.MmkvManager.decodeServerConfig
 import com.neko.v2ray.handler.MmkvManager.decodeServerList
 import com.neko.v2ray.util.JsonUtil
 import com.neko.v2ray.util.Utils
+=======
+import com.neko.v2ray.AppConfig
+import com.neko.v2ray.AppConfig.ANG_PACKAGE
+import com.neko.v2ray.AppConfig.GEOIP_PRIVATE
+import com.neko.v2ray.AppConfig.GEOSITE_PRIVATE
+import com.neko.v2ray.AppConfig.TAG_DIRECT
+import com.neko.v2ray.dto.EConfigType
+import com.neko.v2ray.dto.Language
+import com.neko.v2ray.dto.ProfileItem
+import com.neko.v2ray.dto.RoutingType
+import com.neko.v2ray.dto.RulesetItem
+import com.neko.v2ray.dto.V2rayConfig
+import com.neko.v2ray.dto.VpnInterfaceAddressConfig
+import com.neko.v2ray.handler.MmkvManager.decodeServerConfig
+import com.neko.v2ray.handler.MmkvManager.decodeServerList
+import com.neko.v2ray.util.JsonUtil
+import com.neko.v2ray.util.Utils
+>>>>>>> 2fb6e62e (Added setting option for VPN interface address):V2rayNG/app/src/main/java/com/v2ray/ang/handler/SettingsManager.kt
 import java.io.File
 import java.io.FileOutputStream
 import java.util.Collections
@@ -270,6 +289,9 @@ object SettingsManager {
         }
     }
 
+    /**
+     * Set night mode.
+     */
     // fun setNightMode() {
         // when (MmkvManager.decodeSettingsString(AppConfig.PREF_UI_MODE_NIGHT, "0")) {
             // "0" -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
@@ -277,4 +299,17 @@ object SettingsManager {
             // "2" -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
         // }
     // }
+
+    /**
+     * Retrieves the currently selected VPN interface address configuration.
+     * This method reads the user's preference for VPN interface addressing and returns
+     * the corresponding configuration containing IPv4 and IPv6 addresses.
+     *
+     * @return The selected VpnInterfaceAddressConfig instance, or the default configuration
+     *         if no valid selection is found or if the stored index is invalid.
+     */
+    fun getCurrentVpnInterfaceAddressConfig(): VpnInterfaceAddressConfig {
+        val selectedIndex = MmkvManager.decodeSettingsString(AppConfig.PREF_VPN_INTERFACE_ADDRESS_CONFIG_INDEX, "0")?.toInt()
+        return VpnInterfaceAddressConfig.getConfigByIndex(selectedIndex ?: 0)
+    }
 }